bd8e9986166a7112dee2db52ae045a8d9c1c831f
Joost <bol0004@hz.nl>
2021-06-09 20:13:52 +0200

    Added price variable

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -18,6 +18,8 @@ app.component('product-display', {
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
 
+        <p>Price: {{ price }}</p>
+
         <p>Shipping: {{ shipping }}</p>
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
@@ -49,8 +51,8 @@ app.component('product-display', {
         selectedVariant: 0,
         details: ['A graphics processing unit (GPU) is a specialized electronic circuit designed to rapidly manipulate and alter memory to accelerate the creation of images in a frame buffer intended for output to a display device. GPUs are used in embedded systems, mobile phones, personal computers, workstations, and game consoles.'],
         variants: [
-          { id: 2234, color: '#ED1C24', image: './assets/images/ATI HD 5450.jpg', quantity: 50, brand: 'AMD', product: 'ATI HD 5450'},
-          { id: 2235, color: '#76b900', image: './assets/images/RTX3080.jpg', quantity: 0, brand: 'Nvidia', product: 'RTX 3080'},
+          { id: 2234, color: '#ED1C24', image: './assets/images/ATI HD 5450.jpg', quantity: 50, brand: 'AMD', product: 'ATI HD 5450', price: "$299"},
+          { id: 2235, color: '#76b900', image: './assets/images/RTX3080.jpg', quantity: 0, brand: 'Nvidia', product: 'RTX 3080', price: "The soul of your firstborn"},
         ],
         reviews: []
     }
@@ -72,6 +74,9 @@ app.component('product-display', {
       },
       image() {
           return this.variants[this.selectedVariant].image
+      },
+      price() {
+        return this.variants[this.selectedVariant].price
       },
       inStock() {
           return this.variants[this.selectedVariant].quantity
